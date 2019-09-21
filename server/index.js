752d67e71bf1ee30157d181ed4d0b559d4f8a08e
Sebastian <seb.namek@gmail.com>
2019-09-21 15:06:14 -0400

    api work done

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const axios = require('axios')
-// const bcrypt = require('bcrypt')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -20,62 +19,44 @@ app.get("/people/:id", (req, res) => {
     console.log(`requesting id ${id}`)
     axios.get(`http://swapi.co/api/people/${id}`)
     .then(({ data }) => {
-        res.json(data)
+        const speciesURL = data.species[0]
+        const films = data.films
+        const speciesPromise = getSepcies(speciesURL)
+        const filmPromise = getFilms(films)
+        Promise.all([filmPromise, speciesPromise])
+        .then(prom => {
+            data.filmsArr = prom[0]
+            data.speciesInfo = prom[1]
+            res.json(data)
+        })
     })
+    .catch(err => console.error('something went wrong\n', err))
 })
 
-// app.post("/user", (req, res, next) => {
-//     const { first_name, last_name, email, password } = req.body
-//     bcrypt.hash(password, 10, (err, hash) => {
-//         return User.create({
-//             first_name,
-//             last_name,
-//             email,
-//             password: hash
-//         })
-//         .then(() => res.send('User successfully created'))
-//         .catch(err => {
-//             console.error(err)
-//             next()
-//         })
-//     })
-// })
+function getFilms (films) {
+    const promiseArr = films.map(film => {
+        return axios.get(film)
+        .then(({ data }) => {
+            const { title, director, producer, release_date } = data
+            const filmInfo = {
+                title,
+                director,
+                producer,
+                release_date
+            }
+            return filmInfo
+        })
+    })
+    return Promise.all(promiseArr)
+}
 
-// app.post("/home", function(req, res, next) {
-//     const { longitude, latitude, UserId } = req.body
-//     return Home.create({
-//         longitude: parseFloat(longitude),
-//         latitude: parseFloat(latitude),
-//         UserId
-//     })
-//     .then(() => {
-//         res.end(`Successfully added home location at 
-//         \nlatitude:${latitude}
-//         \nlongitude:${longitude}
-//     `)
-//     })
-//     .catch(err => {
-//         console.error(err)
-//         next()
-//     })
-// })
-
-// app.get("/user", (req, res) => {
-//     res.end()
-//     return User.findAll()
-//     .then(user => {
-//         console.log(user[0].dataValues)
-//         res.send(user[0].dataValues)
-//     })
-// })
-
-// app.get("/home", (req, res) => {
-//     return Home.findAll()
-//     .then(home => {
-//         console.log(home[0].dataValues)
-//         res.status(200).send(home)
-//     })
-// })
+function getSepcies (speciesURL) {
+    return axios.get(speciesURL)
+    .then(({ data }) => {
+        const { name, average_lifespan, classification, language } = data
+        return { name, average_lifespan, classification, language }
+    })
+}
 
 http.createServer(app).listen(4000, function() {
     console.log("Express server listening on port 4000")
